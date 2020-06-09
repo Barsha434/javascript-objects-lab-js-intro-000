9e6ef7109eecc7981217f75933f6b67a9fc79960
Barsha Dahal <dahalbarsha19@gmail.com>
2020-06-09 23:07:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 //returns an object with the orignal key value pairs
 // and the new key value pair
-function updateObjectWithKeyAndValue(object, key, value){
-  object[key]=value
-  return object
-}
+//destructively
+
 // does not modify the original object,
  //but rather returns a clone with
  //the new data
